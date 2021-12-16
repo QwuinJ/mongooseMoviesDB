2e6a5f6e1b7e45d3615d551f8d7e0dbf931fe284
QwuinJ <jackbelfield8@gmail.com>
2021-12-16 10:55:16 +0000

    added other libraries

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,2 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+
+const connection = async () => {
+	try {
+		await mongoose.connect(process.env.MOGNO_URI);
+		console.log('successsfully connected');
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+connection();
